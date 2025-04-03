1aa473b612859a5e906a1e591e0c61958129be29
Correia3d <lcplayer2@gmail.com>
2025-04-03 14:40:28 -0300

    ok1

--- src/webserver.go
+++ src/webserver.go
@@ -1376,8 +1376,8 @@ func TibiaDataHTMLDataCollector(TibiaDataRequest TibiaDataRequestStruct) (string
 	}
 
 	// Set client timeout  and retry
-	client.SetTimeout(3 * time.Second)
-	client.SetRetryCount(3)
+	client.SetTimeout(5 * time.Second)
+	client.SetRetryCount(2)
 
 	// Set headers for all requests
 	client.SetHeaders(map[string]string{
