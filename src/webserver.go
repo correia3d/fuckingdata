570f02db63431fca88cde9a2b2107b6180e8bf4e
Correia3d <lcplayer2@gmail.com>
2025-05-13 12:45:30 -0300

    ok1

--- src/webserver.go
+++ src/webserver.go
@@ -1366,6 +1366,10 @@ func TibiaDataUserAgentGenerator(version int) string {
 
 // TibiaDataHTMLDataCollector func
 func TibiaDataHTMLDataCollector(TibiaDataRequest TibiaDataRequestStruct) (string, error) {
+	// Adiciona um pequeno delay aleatório entre 50ms e 200ms
+	randomDelay := time.Duration(50+rand.Intn(150)) * time.Millisecond
+	time.Sleep(randomDelay)
+
 	// Setting up resty client
 	client := resty.New()
 
@@ -1375,14 +1379,25 @@ func TibiaDataHTMLDataCollector(TibiaDataRequest TibiaDataRequestStruct) (string
 		client.EnableTrace()
 	}
 
-	// Set client timeout  and retry
+	// Set client timeout and retry
 	client.SetTimeout(5 * time.Second)
 	client.SetRetryCount(2)
 
-	// Set headers for all requests
+	// Set headers for all requests - cabeçalhos realistas para parecer navegador normal
 	client.SetHeaders(map[string]string{
-		"Content-Type": "application/json",
-		"User-Agent":   TibiaDataUserAgent,
+		"Content-Type":              "application/json",
+		"User-Agent":                TibiaDataUserAgent,
+		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+		"Accept-Language":           "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
+		"Accept-Encoding":           "gzip, deflate, br",
+		"DNT":                       "1",
+		"Connection":                "keep-alive",
+		"Upgrade-Insecure-Requests": "1",
+		"Sec-Fetch-Dest":            "document",
+		"Sec-Fetch-Mode":            "navigate",
+		"Sec-Fetch-Site":            "none",
+		"Sec-Fetch-User":            "?1",
+		"Cache-Control":             "max-age=0",
 	})
 
 	// Enabling Content length value for all request
